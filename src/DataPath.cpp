e0bc4703d67b6a80f4d6cf0a8956c4fc5a64634d
stepanvanecek <stepanvanecek@gmail.com>
2022-01-03 09:34:59 +0100

    add dataPath class

--- src/DataPath.cpp
+++ src/DataPath.cpp
@@ -1,4 +1,12 @@
- #include "DataPath.hpp"
+#include "DataPath.hpp"
 
- DataPath::DataPath(Component* _source, Component* _target): source(_source), target(_target) {}
- DataPath::DataPath(Component* _source, Component* _target, double _bw, double _latency): source(_source), target(_target), bw(_bw), latency(_latency)
+DataPath::DataPath(Component* _source, Component* _target): source(_source), target(_target) {}
+DataPath::DataPath(Component* _source, Component* _target, double _bw, double _latency): source(_source), target(_target), bw(_bw), latency(_latency) {}
+
+DataPath::DataPath(Component* _source, Component* _target, int _oriented): source(_source), target(_target), oriented(_oriented) {}
+DataPath::DataPath(Component* _source, Component* _target, int _oriented, double _bw, double _latency): source(_source), target(_target), oriented(_oriented), bw(_bw), latency(_latency) {}
+
+void DataPath::Print()
+{
+    cout << "DataPath src: (" << source->GetComponentTypeStr() << ") id " << source->GetId() << ", target: (" << target->GetComponentTypeStr() << ") id " << target->GetId() << " - bw: " << bw << ", latency: " << latency << endl;
+}
--- src/DataPath.hpp
+++ src/DataPath.hpp
@@ -4,22 +4,37 @@
 #include "Topology.hpp"
 #include <map>
 
+#define SYS_TOPO_DATAPATH_NONE 1
+#define SYS_TOPO_DATAPATH_OUTGOING 2
+#define SYS_TOPO_DATAPATH_INCOMING 4
+#define SYS_TOPO_DATAPATH_BIDIRECTIONAL 8
+#define SYS_TOPO_DATAPATH_ORIENTED 16
+
+#define SYS_TOPO_DATAPATH_TYPE_LOGICAL 32
+#define SYS_TOPO_DATAPATH_TYPE_PHYSICAL 64
+
 using namespace std;
 class Component;
 
 class DataPath {
 
 public:
-    DataPath(Component*, Component*);
-    DataPath(Component*, Component*, double bw, double latency);
+    DataPath(Component* _source, Component* _target);
+    DataPath(Component* _source, Component* _target, double _bw, double _latency);
+    DataPath(Component* _source, Component* _target, int _oriented);
+    DataPath(Component* _source, Component* _target, int _oriented, double _bw, double _latency);
 
+    void Print();
     map<string,void*> metadata;
 private:
     Component * source;
     Component * target;
+    int oriented;
 
     double bw;
     double latency;
+
+    int dp_type;
 };
 
 #endif
