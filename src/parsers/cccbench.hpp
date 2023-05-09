0e1141bea1a3045754a2b14f3d3bc959e76b25a3
Vasilis Flouris <vflouris@ics.forth.gr>
2023-05-09 18:23:05 +0300

    Adds parser and example for cccbench

---
+++ src/parsers/cccbench.hpp
@@ -0,0 +1,48 @@
+#ifndef CCCBENCH_PARSER
+#define CCCBENCH_PARSER
+
+#include <vector>
+#include "Topology.hpp"
+#include "DataPath.hpp"
+
+template <typename T>class Vec2DArray
+{
+    Vec2DArray(){}
+    std::vector<T> *array;
+    unsigned int size, xdim, ydim;
+public:
+    Vec2DArray(unsigned xdim, unsigned ydim);
+    std::vector<T> *operator [](unsigned int xindex);
+};
+
+template <typename T>Vec2DArray<T>::Vec2DArray(unsigned int _xdim, unsigned int _ydim)
+{
+    this->size = _xdim * _ydim;
+    this->xdim = _xdim;
+    this->ydim = _ydim;
+    this->array = new std::vector<T> [this->size];
+}
+
+template <typename T>std::vector<T> *Vec2DArray<T>::operator [](unsigned int xindex)
+{
+    return &(this->array[this->xdim*xindex]);
+}
+
+class CccbenchParser{
+    unsigned int firstCore;
+    unsigned int lastCore;
+    unsigned int lines;
+    const char *metric_name = "xylat";
+    const char *xcore_name = "xcore";
+    const char *ycore_name = "ycore";
+    Vec2DArray<float> *c2cDatapoints;
+    CccbenchParser():c2cDatapoints((Vec2DArray<float> *)0){}
+public:
+    virtual ~CccbenchParser(){if(this->c2cDatapoints) {delete [] c2cDatapoints;}}
+    unsigned int xtoi(unsigned int _x){return _x - this->firstCore;}
+    unsigned int ytoi(unsigned int _y){return _y - this->firstCore;}
+    CccbenchParser(const char *csv_path);
+    void applyDataPaths(Component *root);
+};
+
+#endif
