57b5c832a45e291b233d2c3c989d2fdced7684fb
Stepan Vanecek <stepan.vanecek@tum.de>
2023-05-23 11:11:03 +0200

    sys-sage options as #cmakedefine, sys-sage.hpp header as a template

--- src/sys-sage.hpp
+++
@@ -1,8 +0,0 @@
-//includes all other headers
-
-#include "Topology.hpp"
-#include "DataPath.hpp"
-#include "xml_dump.hpp"
-#include "parsers/hwloc.hpp"
-#include "parsers/caps-numa-benchmark.hpp"
-#include "parsers/gpu-topo.hpp"
